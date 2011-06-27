896fcc0700b027acebed531bcf18efa7ec2a1c02
Tássia Camões Araújo <tassia@gmail.com>
2011-06-27 18:19:52 -0300

    Integrated recommender and new web form; changed section about; changes frontpage layout;

--- src/web/server.py
+++ src/web/server.py
@@ -6,6 +6,7 @@ import tempfile
 import sys
 import simplejson as json
 import apt
+import re
 
 sys.path.insert(0,"../")
 
@@ -15,44 +16,24 @@ from user import *
 
 import urllib
 
-class RequestForm(form.Form):
-    def __init__(self):
-        form.Form.__init__(self, \
-            form.File("pkgs_file", size=35, description="Upload file"),
-            form.Textarea("pkgs_list", description="Packages",
-                          rows="4", cols="52"),
-            form.Dropdown('limit', [(5, '05'), (10, '10'), (20, '20')],
-                          description = "Limit"),
-            form.Checkbox("strategy_cb", value=1, checked=False,
-                          description="Content-based"),
-            form.Checkbox("strategy_col", value=1, checked=False,
-                          description="Collaborative"),
-            form.Checkbox("strategy_hybrid", value="True", checked=False,
-                          description="Hybrid"),
-            form.Checkbox("strategy_hybrid_plus", value="True", checked=False,
-                          description="Hybrid plus"),
-            validators = [form.Validator("No packages list provided.",
-                                  lambda f: f.has_key("pkgs_list") |
-                                            f.has_key("pkgs_file") ),
-                          form.Validator("No strategy selected.",
-                                  lambda f: f.has_key("strategy_cb") |
-                                            f.has_key("startegy_col") |
-                                            f.has_key("strategy_hybrid") |
-                                            f.has_key("strategy_hybrid_plus")) ])
-
 class FeedbackForm(form.Form):
-    def __init__(self,strategies):
-        desc_dict = {"cta": "Content-based", "col": "Collaborative",
-                     "hybrid": "Hybrib", "hybrid_plus": "Hybrid Plus"}
+    def __init__(self,selected_strategies):
+        desc_dict = {"cb": "Content-based", "cbt": "Content-based",
+                     "cbd": "Content-based", "col": "Collaborative",
+                     "hybrid": "Hybrib"}
         fields = []
-        for strategy in strategies:
+        for strategy in selected_strategies:
             fields.append(form.Radio(desc_dict[strategy],
                 [('1','1 '),('2','2 '),('3','3'),('4','4 '),('5','5')]))
         form.Form.__init__(self, *fields, validators = [])
 
 class Index:
     def GET(self):
-        return render.index("/apprec", "post", RequestForm())
+        return render.index()
+
+class About:
+    def GET(self):
+        return render.about()
 
 class Thanks:
     def POST(self):
@@ -95,52 +76,108 @@ class Package:
             subtags = []
         return debtags
 
+class Request:
+    def __init__(self,web_input,user_id):
+        self.user_id = user_id
+        self.storage = web_input
+
+        self.pkgs_list = []
+        if web_input.has_key('pkgs_list'):
+            self.pkgs_list = web_input['pkgs_list'].encode('utf8').split()
+            print self.pkgs_list
+        print web_input['pkgs_file']
+        if web_input['pkgs_file']:
+            f = open(outputdir + "/packages_list", "wb")
+            lines = web_input['pkgs_file'].file.readlines()
+            print lines
+            if lines[0].startswith('POPULARITY-CONTEST'):
+                del lines[0]
+                del lines[-1]
+                package_name_field = 2
+            else:
+                package_name_field = 0
+            for line in lines:
+                self.pkgs_list.append(line.split()[package_name_field])
+                f.write(line)
+            f.close()
+
+        if web_input.has_key('limit'):
+            self.limit = int(web_input['limit'])
+        if web_input.has_key('profile_size'):
+            self.profile_size = int(web_input['profile_size'])
+        if web_input.has_key('weight'):
+            self.weight = web_input['weight'].encode('utf8')
+
+        self.selected_strategies = []
+        if web_input.has_key('strategy'):
+            if web_input['strategy'].encode('utf8') == "content":
+                if (web_input.has_key('tag') and web_input.has_key('desc')):
+                    self.selected_strategies.append("cb")
+                elif web_input.has_key('desc'):
+                    self.selected_strategies.append("cbd")
+                else:
+                    self.selected_strategies.append("cbt")
+            if web_input['strategy'].encode('utf8') == "collab":
+                self.selected_strategies.append("col")
+            if web_input['strategy'].encode('utf8') == "hybrid":
+                self.selected_strategies.append("hybrid")
+
+        if web_input.has_key('cluster'):
+            self.cluster = bool(web_input.has_key['cluster'].encode('utf8'))
+
+        if web_input.has_key('neighbours'):
+            self.neighbours = int(web_input['neighbours'])
+
+        self.profiles_set = set()
+        if web_input.has_key('profile_desktop'):
+            self.profiles = self.profiles.add("desktop")
+        if web_input.has_key('profile_admin'):
+            self.profiles = self.profiles.add("admin")
+        if web_input.has_key('profile_devel'):
+            self.profiles = self.profiles.add("devel")
+        if web_input.has_key('profile_science'):
+            self.profiles = self.profiles.add("science")
+        if web_input.has_key('profile_arts'):
+            self.profiles = self.profiles.add("arts")
+
+    def __str__(self):
+        return self.storage
+
+    def validates(self):
+        self.errors = []
+        if (not self.pkgs_list or not self.selected_strategies):
+            self.errors.append("No upload file or packages list was provided.")
+            return False
+        if not self.selected_strategies:
+            self.errors.append("No strategy was selected.")
+            return False
+        return True
+
+    def get_strategy_details(self):
+        return self.selected_strategies[0]
 
 class AppRecommender:
     def POST(self):
+        print "post",web.input()
         outputdir = tempfile.mkdtemp(prefix='',dir='./submissions/')
         user_id = outputdir.lstrip('./submissions/')
-        request = RequestForm()
-        request_info = web.input(pkgs_file={})
-        if not request.validates(request_info):
-            return render.error(request)
+        print web.input(pkgs_file={})
+        request = Request(web.input(pkgs_file={}),user_id)
+        if not request.validates():
+            return render.error(request.errors)
         else:
-            user_pkgs_list = []
-            if request_info.has_key('pkgs_list'):
-                user_pkgs_list = request_info['pkgs_list'].encode('utf8').split()
-                print user_pkgs_list
-
-            if request_info['pkgs_file'].value:
-                f = open(outputdir + "/packages_list", "wb")
-                lines = request_info['pkgs_file'].file.readlines()
-                print lines
-                if lines[0].startswith('POPULARITY-CONTEST'):
-                    del lines[0]
-                    del lines[-1]
-                    package_name_field = 2
-                else:
-                    package_name_field = 0
-                for line in lines:
-                    user_pkgs_list.append(line.split()[package_name_field])
-                    f.write(line)
-                f.close()
-
-            strategies = []
-            if request_info.has_key('strategy_cb'): strategies.append("cta")
-            ### Colaborative strategies can not go online yet
-            if request_info.has_key('strategy_col'): strategies.append("col")
-            if request_info.has_key('strategy_hybrid'):
-                strategies.append("hybrid")
-            if request_info.has_key('strategy_hybrid_plus'):
-                strategies.append("hybrid_plus")
-            recommends = self._recommends(user_id,user_pkgs_list,strategies, int(request_info['limit']))
+            recommendation = self._recommends(request)
             ### Getting package summary (short description) ###
             pkg_summaries = {}
             cache = apt.Cache()
-            for strategy, result in recommends.items():
+            for strategy, result in recommendation.items():
                 for pkg in result:
-                    pkg_summaries[pkg] = cache[pkg].candidate.summary
-            return render.apprec(recommends, pkg_summaries, FeedbackForm(strategies))
+                    try:
+                        pkg_summaries[pkg] = cache[pkg].candidate.summary
+                    except:
+                        pkg_summaries[pkg] = ""
+            return render.apprec(recommendation, pkg_summaries,
+                                 FeedbackForm(request.selected_strategies),request)
 
 # parsing json from screenshots - can be usefull in the future...
 #    def _packages_attrs(self, recommends): #recommends is result of _recommends()
@@ -156,18 +193,16 @@ class AppRecommender:
 #                recommended_pkgs_attrs[pkg_attrs_dict['name']] = pkg_attrs_dict
 #        return recommended_pkgs_attrs
 
-    def _recommends(self,user_id,user_pkgs_list,strategies,limit):
-        user = User(dict.fromkeys(user_pkgs_list,1),user_id)
+    def _recommends(self,request):
+        user = User(dict.fromkeys(request.pkgs_list,1),request.user_id)
         user.maximal_pkg_profile()
         cfg = Config()
         rec = Recommender(cfg)
         results = dict()
-        for strategy in strategies:
-            ### Colaborative strategies can not go online yet
-            #eval("rec."+strategy+"(cfg)")
-            rec.cta(cfg)
-            prediction = rec.get_recommendation(user).get_prediction(limit)
-            results[rec.strategy.description] = \
+        for strategy_str in request.selected_strategies:
+            rec.set_strategy(strategy_str)
+            prediction = rec.get_recommendation(user).get_prediction(request.limit)
+            results[strategy_str] = \
                 [result[0] for result in prediction]
         return results
 
@@ -181,9 +216,10 @@ def add_global_hook():
 render = web.template.render('templates/', base='layout')
 render_plain = web.template.render('templates/')
 
-urls = ('/',   			'Index',
-        '/apprec',	  	'AppRecommender',
+urls = ('/',   			    'Index',
+        '/apprec',	  	    'AppRecommender',
         '/thanks',   		'Thanks',
+        '/about',           'About',
         '/package/(.*)',  	'Package'
        )
 
