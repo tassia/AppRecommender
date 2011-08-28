928cc9054b057b5b14ed07d6cb6020d6236e866d
Tássia Camões Araújo <tassia@gmail.com>
2011-08-28 15:55:34 -0300

    Survey refactoring.

--- src/web/survey.py
+++ src/web/survey.py
@@ -8,6 +8,7 @@ import sys
 import simplejson as json
 import apt
 import re
+import socket
 
 sys.path.insert(0,"../")
 
@@ -15,6 +16,7 @@ import logging
 from config import Config
 from recommender import *
 from user import *
+from data import DebianPackage
 
 import urllib
 
@@ -77,64 +79,13 @@ class Package:
             subtags = []
         return debtags
 
-class Request:
-    def __init__(self,web_input,submissions_dir,user_id=0,pkgs_list=0):
-        self.strategy = ""
-        if user_id:
-            self.user_id = user_id
-            self.outputdir = os.path.join(submissions_dir,user_id)
-            logging.info("New round for user %s" % self.user_id)
-        else:
-            self.outputdir = tempfile.mkdtemp(prefix='',dir=submissions_dir)
-            self.user_id = self.outputdir.lstrip(submissions_dir)
-            logging.info("Request from user %s" % self.user_id)
-            logging.debug("Created dir %s" % self.outputdir)
-
-        pkgs_list_file = os.path.join(self.outputdir,"packages_list")
-        if pkgs_list:
-            self.pkgs_list = pkgs_list
-            if not os.path.exists(pkgs_list_file):
-                with open(pkgs_list_file,"w") as f:
-                    for pkg in pkgs_list:
-                        f.write(pkg+"\n")
-        else:
-            self.pkgs_list = []
-            if web_input['pkgs_file'].value:
-                f = open(pkgs_list_file, "w")
-                lines = web_input['pkgs_file'].file.readlines()
-                with open(os.path.join(self.outputdir,"upload"), "w") as upload:
-                    upload.writelines(lines)
-                # popcon submission format
-                if lines[0].startswith('POPULARITY-CONTEST'):
-                    del lines[0]
-                    del lines[-1]
-                    package_name_field = 2
-                else:
-                    package_name_field = 0
-                for line in lines:
-                    self.pkgs_list.append(line.split()[package_name_field])
-                for pkg in self.pkgs_list:
-                    f.write(pkg+'\n')
-                f.close()
-
-    def __str__(self):
-        return "Request %s:\n %s" % (self.user_id,str(self.pkgs_list))
-
-    def validates(self):
-        self.errors = []
-        if not self.pkgs_list:
-            self.errors.append("No packages list provided.")
-        if self.errors:
-            return False
-        return True
-
 class Save:
     def POST(self):
         web_input = web.input()
         logging.info("Saving user evaluation...")
         logging.info(web_input)
         user_id = web_input['user_id'].encode('utf8')
-        with open("./submissions/%s/packages_list" % user_id) as packages_list:
+        with open("./submissions/%s/uploaded_file" % user_id) as packages_list:
             pkgs_list = [line.strip() for line in packages_list.readlines()]
         strategy = web_input['strategy']
         logging.debug("Saving evaluation for user %s, strategy %s and packages..."
@@ -164,10 +115,48 @@ class Save:
         else:
             return render.survey_index()
 
+class Request:
+    def __init__(self,web_input,submissions_dir):
+        self.strategy = ""
+        # Check if it is first round
+        if web_input.has_key('user_id'):
+            self.user_id = web_input['user_id'].encode('utf8')
+            self.user_dir = os.path.join(submissions_dir, self.user_id)
+            logging.info("New round for user %s" % self.user_id)
+        else:
+            self.user_dir = tempfile.mkdtemp(prefix='',dir=submissions_dir)
+            self.user_id = self.user_dir.split("/")[-1]
+            logging.info("Request from user %s" % self.user_id)
+            logging.debug("Created dir %s" % self.user_dir)
+        uploaded_file = os.path.join(self.user_dir,"uploaded_file")
+        if not os.path.exists(uploaded_file):
+            if web_input['pkgs_file'].value:
+                lines = web_input['pkgs_file'].file.readlines()
+                with open(uploaded_file, "w") as uploaded:
+                    uploaded.writelines(lines)
+        with open(uploaded_file) as uploaded:
+            if uploaded.readline().startswith('POPULARITY-CONTEST'):
+                self.user = PopconSystem(uploaded_file,self.user_id)
+            else:
+                self.user = PkgsListSystem(uploaded_file,self.user_id)
+
+    def __str__(self):
+        return "Request %s:\n %s" % (self.user.user_id,
+                                     str(self.user.pkg_profile))
+
+    def validates(self):
+        self.errors = []
+        if not self.user.pkg_profile:
+            self.errors.append("No packages list provided.")
+        if self.errors:
+            return False
+        return True
+
 class Survey:
     def __init__(self):
         logging.info("Setting up survey...")
-        self.rec = Recommender(Config())
+        self.cfg = Config()
+        self.rec = Recommender(self.cfg)
         self.submissions_dir = "./submissions/"
         if not os.path.exists(self.submissions_dir):
             os.makedirs(self.submissions_dir)
@@ -175,25 +164,12 @@ class Survey:
     def POST(self):
         web_input = web.input(pkgs_file={})
         logging.debug("Survey web_input %s" % str(web_input))
-        self.strategies = ["cb","cbd","cbt","col"]
-        # If it is not the first strategy round, save the previous evaluation
-        if not web_input.has_key('user_id'):
-            request = Request(web_input,self.submissions_dir)
-        else:
-            user_id = web_input['user_id'].encode('utf8')
-            with open("./submissions/%s/packages_list" % user_id) as packages_list:
-                pkgs_list = [line.strip() for line in packages_list.readlines()]
-            request = Request(web_input,self.submissions_dir,user_id,pkgs_list)
+        self.strategies = ["demo_cb","demo_cbd","demo_cbt","demo_col"]#,"demo_colco"]
+        request = Request(web_input,self.submissions_dir)
         if not request.validates():
             return render.error_survey()
         else:
-            user = User(dict.fromkeys(request.pkgs_list,1),request.user_id)
-            program_profile = user.filter_pkg_profile(os.path.join(self.rec.cfg.filters,"program"))
-            desktop_profile = user.filter_pkg_profile(os.path.join(self.rec.cfg.filters,"desktop"))
-            if (len(desktop_profile)>10 or
-                len(desktop_profile)>len(program_profile)/2):
-                self.strategies = [strategy_str+"_desktop" for strategy_str
-                                   in self.strategies[:]]
+            # Check the remaining strategies and select a new one
             old_strategies = [dirs for root, dirs, files in
                               os.walk(os.path.join(self.submissions_dir,
                                                    request.user_id))]
@@ -203,34 +179,45 @@ class Survey:
             else:
                 strategies = self.strategies
             if not strategies:
-                return render.thanks(user_id)
+                return render.thanks(request.user_id)
             request.strategy = random.choice(strategies)
             logging.info("Selected \'%s\' from %s" % (request.strategy,strategies))
+            # Get recommendation
             self.rec.set_strategy(request.strategy)
-            prediction = self.rec.get_recommendation(user,10).get_prediction()
-            logging.info("Prediction for user %s" % user.user_id)
+            prediction = self.rec.get_recommendation(request.user,2).get_prediction()
+            logging.info("Prediction for user %s" % request.user_id)
             logging.info(str(prediction))
-            output_dir = ("./submissions/%s/%s/" %
-                          (user.user_id,request.strategy))
-            os.makedirs(output_dir)
-            with open(os.path.join(output_dir,"prediction"),"w") as prediction_file:
+            strategy_dir = os.path.join(request.user_dir,request.strategy)
+            os.makedirs(strategy_dir)
+            with open(os.path.join(strategy_dir,"prediction"),"w") as prediction_file:
                 for pkg,rating in prediction:
                     prediction_file.write("%s %f.2\n" % (pkg,rating))
             logging.debug("Saved %s/%s prediction to file" %
-                          (user.user_id,request.strategy))
+                          (request.user_id,request.strategy))
             recommendation = [result[0] for result in prediction]
-            pkg_summaries = {}
-            pkg_details = []
-            cache = apt.Cache()
-            for pkg in recommendation:
-                try:
-                    logging.debug("Getting details of package %s" % pkg)
-                    pkg_details.append(Package().get_details_from_dde(pkg))
-                    pkg_summaries[pkg] = cache[pkg].candidate.summary
-                except:
-                    pkg_summaries[pkg] = ""
-            if pkg_details:
-                return render.survey(pkg_details, request)
+
+            # Check connection to DDE
+            try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((self.cfg.dde_server,self.cfg.dde_port))
+                dde = 1
+                s.close()
+            except:
+                dde =0
+                logging.debug("Could not connect to dde")
+            # Load packages details
+            pkgs_details = []
+            for pkg_name in recommendation:
+                logging.info("Getting details of package %s" % pkg_name)
+                pkg = DebianPackage(pkg_name)
+                if dde:
+                    pkg.load_details_from_dde(self.cfg.dde_server,self.cfg.dde_port)
+                else:
+                    pkg.load_details_from_apt()
+                pkgs_details.append(pkg)
+            if pkgs_details:
+                logging.info("Rendering survey slide...")
+                return render.survey(pkgs_details, request)
             else:
                 return render.error_survey()
 
@@ -241,7 +228,7 @@ def add_global_hook():
         return handler()
     return _wrapper
 
-render = web.template.render('templates/', base='layout')
+render = web.template.render('templates/', base='layout', globals={'hasattr':hasattr})
 render_plain = web.template.render('templates/')
 
 urls = ('/',   		        'Index',
