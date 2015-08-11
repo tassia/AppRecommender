a82697496abe986026016abf91be0ff09e170b83
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2015-08-11 20:40:11 -0300

    Add dde repository and installation dependencies

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#Basic dependencies
 apt-get update
 apt-get install vim -y
-apt-get install python python-xapian python-apt python-cluster python-webpy python-simplejson python-numpy apt-xapian-index app-install-data python-xdg -y
+
+#AppRecommender dependencies
+apt-get install python python-xapian python-apt python-cluster python-webpy python-simplejson python-numpy apt-xapian-index python-xdg -y
+
+#Debian Data Export (DDE) dependencies
+apt-get install python-jsonschema python-yaml python-paste python-debianbts python-werkzeug python-cherrypy -y
