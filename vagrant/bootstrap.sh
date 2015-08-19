d8f19fbad1381b28cb05b928afac6eda11a7ba34
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2015-08-19 10:02:18 -0300

    Remove Debian Data Export dependencies on bootstrap.sh

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -6,6 +6,3 @@ apt-get install vim -y
 
 #AppRecommender dependencies
 apt-get install python python-xapian python-apt python-cluster python-webpy python-simplejson python-numpy apt-xapian-index python-xdg -y
-
-#Debian Data Export (DDE) dependencies
-apt-get install python-jsonschema python-yaml python-paste python-debianbts python-werkzeug python-cherrypy -y
