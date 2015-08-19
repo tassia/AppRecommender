5107b40916d9d6720ffc1671e7646855c44b7b73
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2015-08-19 10:04:36 -0300

    Add /vagrant with default folder in prepare_app_recommender_data.sh

--- prepare_app_recommender_data.sh
+++ prepare_app_recommender_data.sh
@@ -1,28 +1,44 @@
-cd /vagrant
+SRC_DIR=`pwd`
+#PATH_DIR=${1-/vagrant}
+PATH_DIR=$1
 
-echo "Deleting folder: /vagrant/.app-recommender"
-rm -rf .app-recommender
+if [ "$1" == "" ]; then
+  printf "Default folder is '/vagrant', you want use this folder? (Y/n): "
+  read -n 1 input
+  echo ""
 
-echo "Creating folder: /vagrant/.app-recommender"
-mkdir .app-recommender
+  if [ "$input" == "y" ] || [ "$input" == "Y" ] || [ "$input" == "" ]; then
+    PATH_DIR=/vagrant
+  else
+    echo "Use: prepare_app_recommender_data.sh [folder_path]"
+    exit
+  fi
+fi
 
-echo "Creating folder: /vagrant/.app-recommender/filters"
-cd /vagrant/.app-recommender
+echo "Deleting folder: $PATH_DIR/.app-recommender"
+rm -rf $PATH_DIR/.app-recommender
+
+echo "Creating folder: $PATH_DIR/.app-recommender"
+mkdir $PATH_DIR/.app-recommender
+
+echo "Creating folder: $PATH_DIR/.app-recommender/filters"
+cd $PATH_DIR/.app-recommender
 mkdir filters
 
-cd /vagrant/src/bin
+cd $SRC_DIR/src/bin
 
 echo ""
 echo "Creating filter: debtags"
-echo "Running command: $ ./get_tags.sh > ../../.app-recommender/filters/debtags"
-./get_tags.sh > ../../.app-recommender/filters/debtags
+echo "Running command: $ $SRC_DIR/src/bin/get_tags.sh > $PATH_DIR/.app-recommender/filters/debtags"
+./get_tags.sh > $PATH_DIR/.app-recommender/filters/debtags
 
 echo ""
 echo "Creating filter desktopapps"
-echo "Running command: $ ./get_axipkgs.py > ../../.app-recommender/filters/desktopapps"
-./get_axipkgs.py > ../../.app-recommender/filters/desktopapps
+echo "Running command: $ $SRC_DIR/src/bin/get_axipkgs.py > $PATH_DIR/.app-recommender/filters/desktopapps"
+./get_axipkgs.py > $PATH_DIR/.app-recommender/filters/desktopapps
 
 echo ""
 echo "Indexing axi sample: desktopapps"
-echo "Running command: $ ./indexer_axi.py sample ../../.app-recommender/filters/desktopapps"
-./indexer_axi.py sample ../../.app-recommender/filters/desktopapps
+echo "Running command: $ $SRC_DIR/src/bin/indexer_axi.py sample $PATH_DIR/.app-recommender/filters/desktopapps"
+./indexer_axi.py sample $PATH_DIR/.app-recommender/filters/desktopapps
+
