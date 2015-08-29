ced99ed68c34d7f921de38b1432a862110d01037
Lucas Moura <lucas.moura128@gmail.com>
2015-08-29 17:47:36 -0300

    Created setup.py file
    In order to better run the test suite, a setup.py file was created on the root
    directory that contains the path for the test suite.
    
    Signed-off-by: Lucas Moura <lucas.moura128@gmail.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apprecommender',
+    description="Package recommender for GNU packages",
+    version='0.1',
+    url='https://github.com/tassia/AppRecommender',
+    author='Tassia Camoes Araujo',
+    author_email='tassia@acaia.ca',
+    license='GPLv3.txt',
+    packages=find_packages(),
+    test_suite="src.tests",
+    )
