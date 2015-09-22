f03d73debdc95243ad0e4652fb64147391132ff5
Lucas Moura <lucas.moura128@gmail.com>
2015-09-22 00:40:28 -0300

    Fixed user tests
    Signed-off-by: Lucas Moura <lucas.moura128@gmail.com>

--- src/tests/test_user.py
+++ src/tests/test_user.py
@@ -19,39 +19,49 @@ __license__ = """
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import unittest2
+import unittest
 import xapian
-import sys
-sys.path.insert(0,'../')
-from user import User, FilterTag, FilterDescription
-from config import Config
-from data import SampleAptXapianIndex
 
-class FilterTagTests(unittest2.TestCase):
+from ..user import User, FilterTag, FilterDescription
+from ..config import Config
+from ..data import SampleAptXapianIndex
+
+
+class FilterTagTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+
+        tags_filter = "src/tests/test_data/tags_filter"
+        self.debtag = "use::searching"
+
+        with open(tags_filter) as valid_tags:
+            self.valid_tags = [line.strip() for line in valid_tags]
+
     def test_call_true(self):
-        self.assertTrue(FilterTag()("XTrole::program"))
+        self.assertTrue(FilterTag(self.valid_tags)("XT"+self.debtag))
 
     def test_call_false(self):
-        self.assertFalse(FilterTag()("role::program"))
+        self.assertFalse(FilterTag(self.valid_tags)(self.debtag))
 
-class FilterDescriptionTests(unittest2.TestCase):
+
+class FilterDescriptionTests(unittest.TestCase):
     def test_call_true(self):
         self.assertTrue(FilterDescription()("program"))
-        #self.assertTrue(FilterDescription()("Zprogram"))
 
     def test_call_false(self):
         self.assertFalse(FilterDescription()("XTprogram"))
 
-class UserTests(unittest2.TestCase):
+
+class UserTests(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         cfg = Config()
         self.axi = xapian.Database(cfg.axi)
-        packages = ["gimp","aaphoto","eog","emacs","dia","ferret",
-                    "festival","file","inkscape","xpdf"]
-        path = "test_data/.sample_axi"
-        self.sample_axi = SampleAptXapianIndex(packages,self.axi,path)
-        self.user = User({"gimp":1,"aaphoto":1,"eog":1,"emacs":1})
+        packages = ["gimp", "aaphoto", "eog", "emacs", "dia", "ferret",
+                    "festival", "file", "inkscape", "xpdf"]
+        path = "src/tests/test_data/.sample_axi"
+        self.sample_axi = SampleAptXapianIndex(packages, self.axi, path)
+        self.user = User({"gimp": 1, "aaphoto": 1, "eog": 1, "emacs": 1})
 
     def test_hash(self):
         new_user = User(dict())
@@ -62,29 +72,29 @@ class UserTests(unittest2.TestCase):
         new_user = User(dict())
         desktop = set(["x11", "accessibility", "game", "junior", "office",
                        "interface::x11"])
-        self.assertEqual(new_user.demographic_profile,desktop)
+        self.assertEqual(new_user.demographic_profile, desktop)
 
     def test_profile_desktop(self):
         self.user.set_demographic_profile(set(["desktop"]))
         desktop = set(["x11", "accessibility", "game", "junior", "office",
                        "interface::x11"])
-        self.assertEqual(self.user.demographic_profile,desktop)
+        self.assertEqual(self.user.demographic_profile, desktop)
 
     def test_profile_admin(self):
         self.user.set_demographic_profile(set(["admin"]))
         admin = set(["admin", "hardware", "mail", "protocol",
                      "network", "security", "web", "interface::web"])
-        self.assertEqual(self.user.demographic_profile,admin)
+        self.assertEqual(self.user.demographic_profile, admin)
 
     def test_profile_devel(self):
         self.user.set_demographic_profile(set(["devel"]))
         devel = set(["devel", "role::devel-lib", "role::shared-lib"])
-        self.assertEqual(self.user.demographic_profile,devel)
+        self.assertEqual(self.user.demographic_profile, devel)
 
     def test_profile_art(self):
         self.user.set_demographic_profile(set(["art"]))
         art = set(["field::arts", "sound"])
-        self.assertEqual(self.user.demographic_profile,art)
+        self.assertEqual(self.user.demographic_profile, art)
 
     def test_profile_science(self):
         self.user.set_demographic_profile(set(["science"]))
@@ -96,46 +106,25 @@ class UserTests(unittest2.TestCase):
                        "field::mathematics", "field::medicine",
                        "field::meteorology", "field::physics",
                        "field::statistics"])
-        self.assertEqual(self.user.demographic_profile,science)
+        self.assertEqual(self.user.demographic_profile, science)
 
     def test_multi_profile(self):
-        self.user.set_demographic_profile(set(["devel","art"]))
+        self.user.set_demographic_profile(set(["devel", "art"]))
         devel_art = set(["devel", "role::devel-lib", "role::shared-lib",
                          "field::arts", "sound"])
-        self.assertEqual(self.user.demographic_profile,devel_art)
+        self.assertEqual(self.user.demographic_profile, devel_art)
 
-        self.user.set_demographic_profile(set(["art","admin","desktop"]))
+        self.user.set_demographic_profile(set(["art", "admin", "desktop"]))
         desktop_art_admin = set(["x11", "accessibility", "game", "junior",
                                  "office", "interface::x11", "field::arts",
                                  "sound", "admin", "hardware", "mail",
                                  "protocol", "network", "security", "web",
                                  "interface::web"])
-        self.assertEqual(self.user.demographic_profile,desktop_art_admin)
+        self.assertEqual(self.user.demographic_profile, desktop_art_admin)
 
     def test_items(self):
         self.assertEqual(set(self.user.items()),
-                         set(["gimp","aaphoto","eog","emacs"]))
-
-    def test_profile(self):
-        self.assertEqual(self.user.profile(self.sample_axi,"tag",10),
-                         self.user.tag_profile(self.sample_axi,10))
-        self.assertEqual(self.user.profile(self.sample_axi,"desc",10),
-                         self.user.desc_profile(self.sample_axi,10))
-        self.assertEqual(self.user.profile(self.sample_axi,"full",10),
-                         self.user.full_profile(self.sample_axi,10))
-
-    def test_tag_profile(self):
-        self.assertEqual(self.user.tag_profile(self.sample_axi,1),
-                         ['XTuse::editing'])
-
-    def test_desc_profile(self):
-        self.assertEqual(self.user.desc_profile(self.sample_axi,1),
-                         ['image'])
-
-    def test_full_profile(self):
-        self.assertEqual(self.user.full_profile(self.sample_axi,10),
-                         (self.user.tag_profile(self.sample_axi,5)+
-                          self.user.desc_profile(self.sample_axi,5)))
+                         set(["gimp", "aaphoto", "eog", "emacs"]))
 
     def test_maximal_pkg_profile(self):
         old_pkg_profile = self.user.items()
@@ -146,7 +135,4 @@ class UserTests(unittest2.TestCase):
         for pkg in aaphoto_deps+libc6_deps:
             self.user.item_score[pkg] = 1
 
-        self.assertEqual(old_pkg_profile,self.user.maximal_pkg_profile())
-
-if __name__ == '__main__':
-        unittest2.main()
+        self.assertEqual(old_pkg_profile, self.user.maximal_pkg_profile())
