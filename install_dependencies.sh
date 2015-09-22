cd64e5b928f9d59295e1a46d63db13b5dcd847ef
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2015-09-22 18:26:26 -0300

    Update dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,1 +1,1 @@
-sudo apt-get install python python-xapian python-apt python-cluster python-webpy python-simplejson python-numpy apt-xapian-index python-xdg -y
+sudo apt-get install python python-xapian python-apt python-cluster python-webpy python-simplejson python-numpy apt-xapian-index python-xdg debtags -y
