8e39c3c44906ccf77396f0cb70c39651345ee7ac
Lucas Moura <lucas.moura128@gmail.com>
2015-09-29 22:52:15 -0300

    Fixed flake8 for demo_rec.py
    Signed-off-by: Lucas Moura <lucas.moura128@gmail.com>

--- src/bin/demo_rec.py
+++ src/bin/demo_rec.py
@@ -21,7 +21,7 @@ __license__ = """
 
 import os
 import sys
-sys.path.insert(0,'../')
+sys.path.insert(0, '../')
 import commands
 import re
 
@@ -31,6 +31,7 @@ from strategy import PkgMatchDecider
 
 DB_PATH = "/var/lib/debtags/package-tags"
 INDEX_PATH = os.path.expanduser("~/.app-recommender/debtags_index")
+
 
 def load_debtags_db(path):
     """ Load debtags database. """
@@ -44,25 +45,29 @@ def load_debtags_db(path):
         exit(1)
     return debtags_db
 
+
 def get_system_pkgs():
     """ Return set of system packages. """
     dpkg_output = commands.getoutput('/usr/bin/dpkg --get-selections')
-    return dpkg_output.replace('install','\t').split()
+    return dpkg_output.replace('install', '\t').split()
 
-def get_most_relevant_tags(debtags_db,pkgs_list):
+
+def get_most_relevant_tags(debtags_db, pkgs_list):
     """ Return most relevant tags considering a list of packages. """
     relevant_db = debtags_db.choose_packages(pkgs_list)
-    relevance_index = debtags.relevance_index_function(debtags_db,relevant_db)
+    relevance_index = debtags.relevance_index_function(debtags_db, relevant_db)
     sorted_relevant_tags = sorted(relevant_db.iter_tags(),
                                   lambda a, b: cmp(relevance_index(a),
                                                    relevance_index(b)))
     return normalize_tags(' '.join(sorted_relevant_tags[-50:]))
 
+
 def normalize_tags(string):
     """ Normalize tag string so that it can be indexed and retrieved. """
-    return string.replace(':','_').replace('-','\'')
+    return string.replace(':', '_').replace('-', '\'')
 
-def create_debtags_index(debtags_db,index_path):
+
+def create_debtags_index(debtags_db, index_path):
     """ Create a xapian index for debtags info based on file 'debtags_db' and
     place it at 'index_path'.
     """
@@ -71,15 +76,16 @@ def create_debtags_index(debtags_db,index_path):
     print "Creating new debtags xapian index at \'%s\'" % index_path
     debtags_index = xapian.WritableDatabase(index_path,
                                             xapian.DB_CREATE_OR_OVERWRITE)
-    for pkg,tags in debtags_db.iter_packages_tags():
+    for pkg, tags in debtags_db.iter_packages_tags():
         doc = xapian.Document()
         doc.set_data(pkg)
         for tag in tags:
             doc.add_term(normalize_tags(tag))
-        print "indexing ",debtags_index.add_document(doc)
+        print "indexing ", debtags_index.add_document(doc)
     return debtags_index
 
-def load_debtags_index(debtags_db,reindex):
+
+def load_debtags_index(debtags_db, reindex):
     """ Load an existing or new debtags index, based on boolean reindex. """
     if not reindex:
         try:
@@ -90,7 +96,7 @@ def load_debtags_index(debtags_db,reindex):
             print "Could not open debtags xapian index"
             reindex = 1
     if reindex:
-        debtags_index = create_debtags_index(debtags_db,INDEX_PATH)
+        debtags_index = create_debtags_index(debtags_db, INDEX_PATH)
     return debtags_index
 
 
@@ -108,9 +114,9 @@ if __name__ == '__main__':
 
     debtags_db = load_debtags_db(DB_PATH)
     installed_pkgs = get_system_pkgs()
-    best_tags = get_most_relevant_tags(debtags_db,installed_pkgs)
+    best_tags = get_most_relevant_tags(debtags_db, installed_pkgs)
 
-    debtags_index = load_debtags_index(debtags_db,reindex)
+    debtags_index = load_debtags_index(debtags_db, reindex)
     qp = xapian.QueryParser()
     query = qp.parse_query(best_tags)
     enquire = xapian.Enquire(debtags_index)
