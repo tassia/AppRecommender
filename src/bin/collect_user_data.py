9aab1495d375277dd1410536fceeb87891874d64
Lucas Moura <lucas.moura128@gmail.com>
2015-09-30 10:32:30 -0300

    Created way to create file on collect data script
    Signed-off-by: Lucas Moura <lucas.moura128@gmail.com>

--- src/bin/collect_user_data.py
+++ src/bin/collect_user_data.py
@@ -3,15 +3,24 @@
 import os
 
 
-LOG_PATH = '~/app_recommender_log'
+LOG_PATH = os.path.expanduser('~/app_recommender_log')
 
 
 def create_log_folder():
-    os.mkdir(os.path.expanduser(LOG_PATH), 0755)
 
+    if not os.path.exists(LOG_PATH):
+        os.mkdir(LOG_PATH, 0755)
+
+def create_file(file_path):
+
+    if not os.path.exists(file_path):
+        with open(file_path, 'a'):
+            os.utime(file_path, None)
 
 def main():
+
     create_log_folder()
+    create_file(LOG_PATH+"/all_pkgs.txt")
 
 
 if __name__ == '__main__':
