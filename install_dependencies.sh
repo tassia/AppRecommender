46d4c4ca56a38f2dab2521f8061eb99784a44137
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2015-10-02 09:58:01 -0300

    Updated install_dependencies.sh
    Signed-off-by: Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,1 +1,3 @@
-sudo apt-get install python python-xapian python-apt python-cluster python-webpy python-simplejson python-numpy apt-xapian-index python-xdg debtags -y
+sudo apt-get install python python-xapian python-apt python-cluster python-webpy python-simplejson python-numpy apt-xapian-index python-xdg debtags python-pip popularity-contest -y
+
+pip install setuptools
