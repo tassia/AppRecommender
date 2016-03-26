bae97563c9f317cdbdfacb7a23ab7d2faa42f49c
Lucas Moura <lucas.moura128@gmail.com>
2016-03-25 23:09:22 -0300

    Save ml cross validation result into a file

--- bin/ml_cross_validation.py
+++ bin/ml_cross_validation.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
 import sys
+import os
 import logging
+import datetime as dt
 
 sys.path.insert(0, '../')
 
@@ -10,9 +12,15 @@ from src.evaluation import (SimpleAccuracy, Precision, Recall, FPR,
                             F_score)
 
 
+CROSS_VALIDATION_FOLDER = 'cross_validation_data/'
+
+
 def ml_cross_validation():
     logger = logging.getLogger('')
     logger.setLevel(logging.CRITICAL)
+
+    if not os.path.exists(CROSS_VALIDATION_FOLDER):
+        os.mkdir(CROSS_VALIDATION_FOLDER)
 
     partition_size = 0.7
     rounds = 5
@@ -20,11 +28,16 @@ def ml_cross_validation():
     labels = ['EX', 'G', 'M', 'B', 'H']
     threshold = [95, 65, 25, 10, 5]
 
+    cross_validaton_file = 'cross_validation_result_{0}_{1}_{2}'.format(
+        rounds, partition_size, dt.datetime.now().strftime('%Y%m%d%H%M'))
+
     ml_cross_validation = CrossValidationMachineLearning(
         partition_size, rounds, metrics_list, labels, threshold)
 
     ml_cross_validation.run(None)
-    print ml_cross_validation
+
+    with open(CROSS_VALIDATION_FOLDER + cross_validaton_file, 'w') as result:
+        result.write(ml_cross_validation.__str__())
 
 if __name__ == '__main__':
     ml_cross_validation()
