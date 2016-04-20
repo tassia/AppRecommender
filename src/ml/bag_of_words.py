1f6fe633ed05fb45f9d6e5a761c02e50d906a64c
Lucas Moura <lucas.moura128@gmail.com>
2016-04-20 03:42:08 -0300

    Refactor bag of words module

--- src/ml/bag_of_words.py
+++ src/ml/bag_of_words.py
@@ -1,10 +1,12 @@
-import os
 import pickle
 
-from sklearn.feature_extraction.text import CountVectorizer
+from apt import Cache
+
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import GaussianNB
 
 from src.config import Config
+from src.ml.data import MachineLearningData
 
 
 class BagOfWords():
@@ -15,51 +17,77 @@ class BagOfWords():
     MODEL_ALREADY_CREATED = 1
     CREATED_MODEL = 0
 
+    @staticmethod
+    def save(bag_of_words, file_path):
+        with open(file_path, 'wb') as text:
+            pickle.dump(bag_of_words, text)
+
+    @staticmethod
+    def load(file_path):
+        with open(file_path, 'rb') as text:
+            bag_of_words = pickle.load(text)
+
+        return bag_of_words
+
     def __init__(self):
-        self.vectorizer = CountVectorizer(
-            analyzer="word",
-            tokenizer=None,
-            preprocessor=None,
-            stop_words=None,
-            max_features=5000)
+        self.vectorizer = TfidfVectorizer(
+            max_df=0.8,
+            max_features=5000,
+            min_df=3,
+            stop_words='english',
+            use_idf=True)
 
-    def get_description_and_labels(self, pkgs):
-        descriptions, labels = [], []
+    def get_pkgs_classification(self, pkgs_list):
+        pkgs_classification = []
 
-        for description, label in pkgs:
-            descriptions.append(description)
-            labels.append(label)
+        with open(MachineLearningData.PKGS_CLASSIFICATIONS) as pkgs:
+            pkgs_data = pickle.load(pkgs)
 
-        return (descriptions, labels)
+            for pkg_name in pkgs_list:
+                pkgs_classification.append(pkgs_data[pkg_name][-1])
 
-    '''
-    :param descriptions: A list containing all the pkgs descriptions.
-    '''
-    def generate_valid_terms(self, descriptions):
-        train_data_features = self.vectorizer.fit_transform(descriptions)
-        train_data_features = train_data_features.toarray()
+        return pkgs_classification
 
-        return train_data_features
+    def get_pkg_description(self, pkg, cache, ml_data):
+        return ml_data.get_pkg_terms(cache, pkg)
 
-    '''
-    :param pkgs: A list of tuples containg a package description and its
-                 associated label.
-    '''
-    def train_model(self, pkgs):
+    def get_pkg_debtags(self, pkg, axi, ml_data):
+        return ml_data.get_pkg_debtags(axi, pkg)
 
-        if os.path.exists(BagOfWords.BAG_OF_WORDS_MODEL):
-            with open(BagOfWords.BAG_OF_WORDS_MODEL, 'ra') as bag_of_words:
-                self.classifier = pickle.load(bag_of_words)
-                return BagOfWords.MODEL_ALREADY_CREATED
+    def get_pkg_section(self, pkg, cache, ml_data):
+        return ml_data.get_pkg_section(cache, pkg)
 
-        descriptions, labels = self.get_description_and_labels(pkgs)
+    def combine_pkg_info(self, description, debtags, section):
+        description.extend(debtags)
+        description.append(section)
 
-        train_data_features = self.generate_valid_terms(descriptions)
+        return description
+
+    def prepare_data(self, pkg_list, axi, cache, ml_data):
+        pkgs_description = []
+        pkgs_classification = []
+
+        for pkg in pkg_list:
+            description = self.get_pkg_description(pkg, cache, ml_data)
+            debtags = self.get_pkg_debtags(pkg, axi, ml_data)
+            section = self.get_pkg_section(pkg, cache, ml_data)
+
+            pkg_data = self.combine_pkg_info(description, debtags, section)
+            pkgs_description.append(' '.join(pkg_data))
+
+        pkgs_classification = self.get_pkgs_classification(pkg_list)
+
+        return (pkgs_description, pkgs_classification)
+
+    def train_model(self, pkgs_list, axi):
+        cache = Cache()
+        ml_data = MachineLearningData()
+
+        pkgs_description, pkg_classification = self.prepare_data(
+            pkgs_list, axi, cache, ml_data)
+        pkg_features = self.vectorizer.fit_transform(pkgs_description)
 
         self.classifier = GaussianNB()
-        self.classifier.fit(train_data_features, labels)
-
-        with open(BagOfWords.BAG_OF_WORDS_MODEL, 'wa') as bag_of_words:
-            pickle.dump(self.classifier, bag_of_words)
+        self.classifier.fit(pkg_features.toarray(), pkg_classification)
 
         return BagOfWords.CREATED_MODEL
