671e58b5aef77c42340c31cfbcd565b124ebcef6
Lucas Moura <lucas.moura128@gmail.com>
2016-05-01 18:18:23 -0300

    Add pc informations to csv

--- bin/data_collect/load_user_preferences.py
+++ bin/data_collect/load_user_preferences.py
@@ -32,6 +32,28 @@ def load_strategies(folder_path):
     return strategies
 
 
+def load_pc_informations(folder_path):
+    all_files = commands.getoutput("ls {}".format(folder_path)).splitlines()
+    files = [f for f in all_files if f.endswith('informations.txt')]
+
+    informations = {}
+    pc_informations_file = '{}{}'.format(folder_path, files[0])
+    valid_info = set(['distributor_id', 'codename'])
+
+    with open(pc_informations_file, 'rb') as text:
+        for line in text:
+            if ':' not in line:
+                continue
+
+            info = line.split(':')
+            info[0] = info[0].lower().replace(' ', '_')
+
+            if info[0] in valid_info:
+                informations[info[0]] = info[1].strip()
+
+    return informations
+
+
 def get_strategies_score(strategies, user_preferences):
     classifications = {1: 'bad', 2: 'redundant', 3: 'useful',
                        4: 'useful_surprise'}
@@ -71,15 +93,30 @@ def get_all_strategies_score(all_folders_path):
     return all_strategies_score
 
 
-def convert_to_csv(all_strategies_score):
+def get_all_pc_informations(all_folders_path):
+    all_pc_informations = []
+
+    for folder_path in all_folders_path:
+        pc_information = load_pc_informations(folder_path)
+        all_pc_informations.append(pc_information)
+
+    return all_pc_informations
+
+
+def convert_to_csv(all_strategies_score, all_pc_informations):
     rows = []
     possible_strategies = sorted(all_strategies_score[0].keys())
+    pc_info_header = sorted(all_pc_informations[0].keys())
     classifications = ['bad', 'redundant', 'useful', 'useful_surprise']
 
     csv_header = ""
+
     for strategy in possible_strategies:
         for classification in classifications:
             csv_header += '{}_{};'.format(strategy, classification)
+
+    for info in pc_info_header:
+        csv_header += '{};'.format(info)
 
     rows.append(csv_header[:-1])
 
@@ -93,6 +130,15 @@ def convert_to_csv(all_strategies_score):
         row = ';'.join(str(element) for element in row)
         rows.append(row)
 
+    index = 1
+    for pc_informations, row in zip(all_pc_informations, rows[1:]):
+        distributor_id = pc_informations['distributor_id']
+        codename = pc_informations['codename']
+        row = row + ';{};{}'.format(codename, distributor_id)
+
+        rows[index] = row
+        index += 1
+
     return rows
 
 
@@ -100,8 +146,9 @@ def main():
     folder_path = get_folder_path()
     all_folders_path = get_all_folders_path(folder_path)
     all_strategies_score = get_all_strategies_score(all_folders_path)
+    all_pc_informations = get_all_pc_informations(all_folders_path)
 
-    csv_rows = convert_to_csv(all_strategies_score)
+    csv_rows = convert_to_csv(all_strategies_score, all_pc_informations)
     for row in csv_rows:
         print row
 
