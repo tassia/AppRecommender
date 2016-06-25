c34f1d438663473921742d8ad2649fd1f2505361
Lucas Moura <lucas.moura128@gmail.com>
2016-06-25 20:01:00 -0300

    Add python-fuzzywuzzy to install dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,4 @@
-sudo apt-get install python python-xapian python-apt python-cluster python-simplejson python-numpy apt-xapian-index python-xdg debtags python-pip python-sklearn python-matplotlib python-stemmer -y
+sudo apt-get install python python-xapian python-apt python-cluster python-simplejson python-numpy apt-xapian-index python-xdg debtags python-pip python-sklearn python-matplotlib python-stemmer python-fuzzywuzzy -y
 
 sudo update-apt-xapian-index
 
