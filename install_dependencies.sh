3c74cd8bfb343747f17ecbf0a13f05ea97db8190
Gabriel Silva <gabriel93.silva@gmail.com>
2016-06-29 11:23:08 -0300

    Adds sudo back to run builds in shell mode
    Signed-off-by: Gabriel Silva <gabriel93.silva@gmail.com>

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,6 +2,6 @@ sudo apt-get update
 
 sudo apt-get install python python-xapian python-apt python-cluster python-simplejson python-numpy apt-xapian-index python-xdg debtags python-pip python-sklearn python-matplotlib python-stemmer python-fuzzywuzzy -y
 
-update-apt-xapian-index
+sudo update-apt-xapian-index
 
-pip install setuptools
+sudo pip install setuptools
