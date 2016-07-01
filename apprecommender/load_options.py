dfb67dff32a1d96b06d504241deab2a619a971a6
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2016-07-01 08:52:04 -0300

    Clear usage options

--- apprecommender/load_options.py
+++ apprecommender/load_options.py
@@ -15,10 +15,10 @@ class LoadOptions(Singleton):
 
     def load(self):
         config = Config()
-        short_options = 'hdvo:f:b:a:e:p:m:u:l:c:x:w:s:z:r:n:idvo:tdvo'
-        long_options = ['help', 'debug', 'verbose', 'kmedoids=', 'maxpopcon=',
-                        'weight=', 'strategy=', 'profile_size=', 'init',
-                        'train', 'because', 'nrecommendation']
+        short_options = 'hdvo:d:v:s:z:idvo:tdvo:b:n'
+        long_options = ['help', 'debug', 'verbose', 'strategy=',
+                        'profile_size=', 'init', 'train', 'because',
+                        'nrecommendation']
         try:
             opts, args = getopt.getopt(sys.argv[1:], short_options,
                                        long_options)
@@ -68,38 +68,10 @@ class LoadOptions(Singleton):
               " generated a given recommendation"
         print "  -d, --debug                Set logging level to debug"
         print "  -v, --verbose              Set logging level to verbose"
-        print "  -o, --output=PATH          Path to file to save output"
-        print ""
-        print " [ data sources ]"
-        print "  -f, --filtersdir=PATH      Path to filters directory"
-        print "  -b, --pkgsfilter=FILTER    File containing packages" \
-              "to be considered for recommendations"
-        print "  -a, --axi=PATH             Path to apt-xapian-index"
-        print "  -p, --popconindex=PATH     Path to popcon index"
-        print "  -e, --dde=URL              DDE url"
-
-        # deprecated options
-        # print " -m, --popcondir=PATH    Path to popcon submissions dir"
-        # print " -u, --indexmode=MODE    " \
-        #        "'old'|'reindex'|'cluster'|'recluster'"
-        # print " -l, --clustersdir=PATH  Path to popcon clusters dir"
-        # print " -c, --medoids=k         " \
-        #        "Number of medoids for clustering"
-        # print " -x, --maxpopcon=k       " \
-        #        "Number of submissions to be considered"
-
         print ""
         print " [ recommender ]"
-        print "  -w, --weight=OPTION        Search weighting scheme"
         print "  -s, --strategy=OPTION      Recommendation strategy"
         print "  -z, --profilesize=k        Size of user profile"
-        print "  -r, --profiling=OPTION     Profile filter strategy"
-        print "  -n, --neighbors=k          " \
-              "Size of neighborhood for collaboration"
-        print ""
-        print " [ weight options ] "
-        print "  trad = traditional probabilistic weighting"
-        print "  bm25 = bm25 weighting scheme"
         print ""
         print " [ strategy options ] "
         print "  cb = content-based, mixed profile"
@@ -111,15 +83,5 @@ class LoadOptions(Singleton):
         print "  cbt_eset = cbt with eset profiling"
         print "  cbd_eset = cbd_eset with eset profiling"
         print "  cbh_eset = cbh with eset profiling"
-        print "  knn = collaborative, tf-idf knn"
-        print "  knn_plus = collaborative, tf-idf weighted knn"
-        print "  knn_eset = collaborative, eset knn"
-        print "  knnco = collaborative through content"
-        print "  knnco_eset = collaborative through content," \
-              " eset recommendation"
         print "  mlbva = machine_learning, Binary Vector Approach"
         print "  mlbow = machine_learning, Bag Of Words"
-        print ""
-        print " [ to train machine learning ] "
-        print "  on path '/bin' run the script" \
-              " 'apprec_ml_traning.py'"
