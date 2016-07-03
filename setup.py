b0937fd0e9835102f5e8d43e07d8a654b700c267
Lucas Moura <lucas.moura128@gmail.com>
2016-07-03 06:12:26 -0300

    Bump version on setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='apprecommender',
     description="Package recommender for GNU packages",
-    version='0.1',
+    version='0.6.1.2',
     url='https://github.com/tassia/AppRecommender',
     author='Tassia Camoes Araujo',
     author_email='tassia@acaia.ca',
